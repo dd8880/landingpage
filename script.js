e76240976f140bf281c0768ddb65297732a56280
Diana <dian5885@stud.kea.dk>
2022-08-07 16:31:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("promotional");
+var btn = document.getElementById("pbtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
